28dff0fb88fbd4cc8838fcaa5c1021ffb2eec1e8
kabikaAsyst <dmkabika@asystresources.com>
2023-08-04 17:32:15 +0200

    parametrage

---
+++ authenticate/config/databaseConfig.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const databaseConnect = () => {
+
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(
+      () => {
+        console.log("CONNEXION A LA BASE DES DONNEES EFFECTUEES AVEC SUCCESS");
+      },
+      (error) => {
+        console.log("PROBLEME D'ACCES A LA BASE DES DONNEES");
+      }
+    );
+};
+
+module.exports = databaseConnect;
